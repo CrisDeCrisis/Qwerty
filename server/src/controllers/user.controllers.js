d69f1e183ee6395c270af7e3e130f3e682f4cec2
sr-roboto <marcoslopez123.lu@gmail.com>
2024-08-21 01:43:42 -0300

    Corregir controlador de registro de usuarios y agregar controlador de inicio de sesión

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import { userServices } from '../services/user.services.js';
+import { userServices } from '../services/user.services.js';
 export const userControllers = {};
 
 userControllers.registerUser = async (req, res) => {
@@ -7,6 +8,16 @@ userControllers.registerUser = async (req, res) => {
     const response = await userServices.registerUser(user);
     res.json(response);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+};
+
+userControllers.loginUser = async (req, res) => {
+  try {
+    const user = req.body;
+    const response = await userServices.loginUser(user);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
