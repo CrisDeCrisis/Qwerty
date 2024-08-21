11152d5b8cf4a83d02f721351b0b1254e2ea8424
sr-roboto <marcoslopez123.lu@gmail.com>
2024-08-21 00:49:43 -0300

    Añadir rutas de registro y login para usuarios

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { userRoutes } from './routes/user.routes.js';
 
 export const app = express();
 
 app.use(express.json);
 app.use(morgan('dev'));
 app.use(cors());
+
+app.use('/user', userRoutes);
