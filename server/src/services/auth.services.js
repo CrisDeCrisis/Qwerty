53039a4ad042d372a6ea311fd60cf22757aace05
sr-roboto <marcoslopez123.lu@gmail.com>
2024-08-21 01:39:29 -0300

    Implementar servicio de autenticación de usuarios

---
+++ server/src/services/auth.services.js
@@ -0,0 +1,32 @@
+import bcrypt from 'bcryptjs';
+import { userModel } from '../models/user.models.js';
+import { generarJWT } from '../helpers/generateJWT.js';
+
+export const authServices = {};
+
+authServices.loginUser = async (user) => {
+  const { Email, Contrasenia } = user;
+  const userExists = await userModel.findOne({ Email });
+  // Verificar que el email exista en la base de datos
+  if (!userExists) {
+    throw new Error('El email no está registrado');
+  }
+  // Verificar que el email no sea nulo
+  if (!Email) {
+    throw new Error('El campo Email es obligatorio');
+  }
+  if (!Contrasenia) {
+    throw new Error('El campo Contrasenia es obligatorio');
+  }
+  // Verificar que la contraseña sea correcta
+  const validPassword = bcrypt.compareSync(Contrasenia, userExists.Contrasenia);
+  if (!validPassword) {
+    throw new Error('La contraseña o el email son incorrectos');
+  }
+  // Generar el token
+  const token = await generarJWT({ id: userExists._id });
+  return {
+    message: 'Usuario logueado',
+    token,
+  };
+};
