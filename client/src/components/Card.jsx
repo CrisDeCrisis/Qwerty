7aa74fae89d46fb30c14583846cb11ec72e8d43d
sr-roboto <marcoslopez123.lu@gmail.com>
2024-08-23 14:52:25 -0300

    Agregar funcionalidad a boton contactar

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,38 +2,36 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+const Cards = React.forwardRef(
+  ({ NombreUsuario, Genero, Pais, Email, Roles, TipoSangre }, ref) => {
+    const cardsBody = (
+      <Card.Body>
+        <Card.Title>{NombreUsuario}</Card.Title>
+        <Card.Text>{Genero}</Card.Text>
+        <Card.Text>{Pais}</Card.Text>
+        <Card.Text>{Roles}</Card.Text>
+        <Card.Title>Grupo sanguíneo: {TipoSangre}</Card.Title>
+        <Button variant="primary" className="fw-bold mt-2" id="btn-login">
+          <a
+            className="text-decoration-none text-white"
+            href={`mailto:${Email}`}
+          >
+            Contactar
+          </a>
+        </Button>
+      </Card.Body>
+    );
 
-const Cards = React.forwardRef(({NombreUsuario, Genero, Pais, Roles, TipoSangre}, ref) => {
-  const cardsBody = (
-    
-       <Card.Body>
-         <Card.Title>{NombreUsuario}</Card.Title>
-         <Card.Text>
-          {Genero}
-         </Card.Text>
-         <Card.Text>
-          {Pais}
-         </Card.Text>
-         <Card.Text>
-          {Roles}
-         </Card.Text>
-         <Card.Title>Grupo sanguíneo: {TipoSangre}</Card.Title>
-         <Button variant="primary" className='fw-bold mt-2' id='btn-login'>Contactar</Button>
-       </Card.Body>
-     
-  )
+    const content = ref ? (
+      <Card ref={ref} style={{ width: '15rem' }}>
+        {cardsBody}
+      </Card>
+    ) : (
+      <Card style={{ width: '15rem' }}>{cardsBody}</Card>
+    );
 
-  const content = ref ? (
-    <Card ref={ref} style={{ width: "15rem" }}>
-      {cardsBody}
-    </Card>
-  ) : (
-    <Card style={{ width: "15rem" }}>
-      {cardsBody}
-    </Card>
-  );
-
-  return content;
-})
+    return content;
+  }
+);
 
 export default Cards;
